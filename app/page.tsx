55d883fd48aea0ebffd02cb27caec5b715aeb09b
“heffo42” <davidheff39@gmail.com>
2024-08-12 21:28:20 -0400

    microsoft

--- app/page.tsx
+++ app/page.tsx
@@ -56,10 +56,7 @@ const MicrosoftAuthComponent: React.FC<{ onSignIn: (username: string) => void }>
       clientId="baf7a588-35f1-4d62-a7a8-c3891541a28c"
       authCallback={authHandler}
       redirectUri="http://localhost:3000/api/auth/callback"
-      buttonTheme="light">
-    
-
-    </MicrosoftLogin>
+      buttonTheme="light" >{}</MicrosoftLogin>
   );
 };
 
